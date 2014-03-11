1bea2b8751d07e7c523dd8ce675eb788115d3507
Carlos Souza <carloshrsouza@gmail.com>
2014-03-11 13:07:49 -0400

    Add seed data to sql

--- structure.sql
+++ structure.sql
@@ -20,12 +20,11 @@ SET default_with_oids = false;
 --
 -- Name: episodes; Type: TABLE; Schema: public; Owner: -; Tablespace:
 --
+DROP TABLE IF EXISTS episodes;
 CREATE TABLE episodes (
     id integer NOT NULL,
     title character varying(255),
-    description text,
-    created_at timestamp without time zone,
-    updated_at timestamp without time zone
+    description text
 );
 --
 -- Name: episodes_id_seq; Type: SEQUENCE; Schema: public; Owner: -
@@ -47,3 +46,8 @@ ALTER TABLE ONLY episodes ALTER COLUMN id SET DEFAULT nextval('episodes_id_seq':
 ALTER TABLE ONLY episodes
     ADD CONSTRAINT episodes_pkey PRIMARY KEY (id);
 SET search_path TO "$user",public;
+
+INSERT INTO episodes (title, description) VALUES
+  ('Winter is Coming', 'Introduces the setting and the main characters of the show.'),
+  ('The Kings Road', 'Stark and his daughters accompany the kings entourage to Kings Landing'),
+  ('Lord Snow', 'Jon Snow trains at The Wall');
