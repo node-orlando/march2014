5b37074d8ed264cdedd4538e1b96a221332135fa
Carlos Souza <carloshrsouza@gmail.com>
2014-03-12 21:07:31 -0400

    Add mongoose example.

---
+++ mongoose1.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/node_episodes_mongoose');
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var EpisodeSchema = new Schema({
+    id:  ObjectId,
+    title: String,
+    description: String
+});
+
+var Episode = mongoose.model('Episode', EpisodeSchema);
+
+var episode = new Episode({
+  title: 'Winter is Coming, yo!'
+});
+
+episode.save(function (err, record) {
+  console.log('Saved: ' + record);
+  mongoose.connection.close();
+});
