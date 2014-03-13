0e573e0883381a7dcb8ec62c733b4bb9ea4d994b
Carlos Souza <carloshrsouza@gmail.com>
2014-03-13 02:01:44 -0400

    Add shared connection.

--- mongoose_express_app/app.js
+++ mongoose_express_app/app.js
@@ -1,6 +1,10 @@
 /**
  * Module dependencies.
  */
+var mongoose = require('mongoose');
+global.db = mongoose.
+  createConnection('mongodb://localhost:27017/node_episodes_mongoose_express');
+global.Schema = mongoose.Schema;
 
 var express = require('express');
 var routes = require('./routes');
--- mongoose_express_app/models/episode.js
+++ mongoose_express_app/models/episode.js
@@ -1,19 +1,13 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/node_episodes_mongoose_express');
-
 // TODO: drop collection in order to start with blank slate.
 
-var Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
-
-var EpisodeSchema = new Schema({
-  id: ObjectId,
+var EpisodeSchema = new global.Schema({
+  id: global.Schema.ObjectId,
   title: String,
   description: String
 });
 
 // http://mongoosejs.com/docs/api.html#index_Mongoose-model
-var Episode = mongoose.model('Episode', EpisodeSchema);
+var Episode = db.model('Episode', EpisodeSchema);
 // use optional third argument to specifcy collection name, instead of having
 // mongoose induce it from the model name.
 
@@ -27,6 +21,8 @@ Episode.create([
     title: 'Lord Snow',
     description: 'Knows nothing.'
   }
-]);
+], function (err, records) {
+  console.log('Saved: ' + records);
+});
 
 module.exports = Episode;
