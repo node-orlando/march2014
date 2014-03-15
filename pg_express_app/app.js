0a097cb6e635e4960b8949b08d5584bf665d62aa
Carlos Souza <carloshrsouza@gmail.com>
2014-03-14 21:40:40 -0400

    Clean up unused.

--- pg_express_app/app.js
+++ pg_express_app/app.js
@@ -2,14 +2,6 @@
  * Module dependencies.
  */
 
-var Bookshelf = require('bookshelf');
-global.db = Bookshelf.initialize({
-  client: 'pg',
-  connection: {
-    database: 'node_episodes'
-  }
-});
-
 var express = require('express');
 var routes = require('./routes');
 var episode = require('./routes/episode');
@@ -27,7 +19,6 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(require('connect-method-override-get')('_method'));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -38,16 +29,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-
 app.get('/episodes', episode.list);
-
-/*
-app.get('/episodes/new', episode.new);
-app.post('/episodes', episode.create);
-app.get('/episodes/:id/edit', episode.edit);
-app.patch('/episodes/:id', episode.update);
-app.del('/episodes/:id', episode.destroy);
-*/
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
