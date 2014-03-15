0341a6f7790d27b0618e48119cd92ced39cdef71
Carlos Souza <carloshrsouza@gmail.com>
2014-03-15 18:51:19 -0400

    Check for errors.

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
-var pg = require("pg");
-var settings = "pg://localhost:5432/node_episodes";
+// File: db.js
+var pg = require('pg');
+var settings = 'pg://localhost:5432/node_episodes';
 var db = {};
 
 db.query = function (queryString, args, cb) {
   pg.connect(settings, function (err, client, done) {
+    if(err) throw err;
+
     client.query(queryString, args, function (err, result) {
+      if(err) throw err;
+
+      done();
       cb(result.rows);
-      done('ok');
     })
   });
 }
